a7809b7baae38e6ed221830e1747ebc6cbd0862e
Nilexpr <just4register2@gmail.com>
2025-08-19 09:36:30 +0000

    feat: add turbo & astro example

---
+++ README.md
@@ -0,0 +1,7 @@
+# 原则
+
+主要用于个人项目，目前第一版先按照 [react-starter-kit](https://github.com/kriasoft/react-starter-kit) 的结构去搭。
+
+# 技术栈
+
+-
--- scripts/git-info-setup.sh
+++ scripts/git-info-setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # setup-git-config.sh
 
-echo "�� 获取GitHub用户信息..."
+echo "1. 获取GitHub用户信息..."
 GITHUB_USER=$(gh api user --jq .login)
 GITHUB_EMAIL=$(gh api user/emails --jq '.[0].email')
 
-echo "⚙️  设置Git配置..."
+echo "2. 设置Git配置..."
 git config --global user.name "$GITHUB_USER"
 git config --global user.email "$GITHUB_EMAIL"
 
-echo "✅ 配置完成："
+echo "3. 配置完成："
 echo "用户名: $(git config user.name)"
 echo "邮箱: $(git config user.email)"
