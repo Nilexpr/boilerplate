cfb1218b4d80df8373b13a551676a164e1e2e61d
Nilexpr <just4register2@gmail.com>
2025-08-27 06:08:02 +0000

    feat: add prettier & create new preset for tailwind

--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -9,11 +9,15 @@ export const Route = createFileRoute("/")({
 function IndexComponent() {
   return (
     <div class="text-center">
-      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold text-white uppercase transition duration-200 ease-in-out hover:bg-gray-900">
         Button
       </button>
       <Tooltip />
       <Accordion />
+      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Submit
+      </button>
+
       <button class="btn btn-primary text-left">Button</button>
     </div>
   );
---
+++ packages/tailwind-preset/README.md
@@ -0,0 +1,1 @@
+仿照 https://github.com/viktorbonino/radix-themes-tw 来复刻一下 Shadcn 的样式
