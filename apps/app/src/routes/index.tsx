c679b509b159577bd64f42b8c403e95c16709d28
Nilexpr <just4register2@gmail.com>
2025-09-01 09:52:56 +0000

    feat: add story

--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/solid-router";
-import { Tooltip } from "../components/dialog";
 import { Accordion } from "../components/accordion";
 
 export const Route = createFileRoute("/")({
@@ -9,16 +8,7 @@ export const Route = createFileRoute("/")({
 function IndexComponent() {
   return (
     <div class="text-center">
-      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold text-white uppercase transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button>
-      <Tooltip />
       <Accordion />
-      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        Submit
-      </button>
-
-      <button class="btn btn-primary text-left">Button</button>
     </div>
   );
 }
--- packages/components/components/accordion/index.tsx
+++ packages/components/components/accordion/index.tsx
@@ -1,3 +1,4 @@
+import { Component, JSX } from "solid-js";
 import * as accordion from "@zag-js/accordion";
 import { normalizeProps, useMachine } from "@zag-js/solid";
 import { createMemo, createUniqueId, For } from "solid-js";
@@ -8,22 +9,28 @@ const data = [
   { title: "Aircraft", content: "Sample accordion content" },
 ];
 
-export function Accordion() {
-  const service = useMachine(accordion.machine, { id: createUniqueId() });
+export const Accordion: Component<{
+  machineProps: Omit<accordion.Props, "id">;
+  item: (accordion.ItemProps & { content: JSX.Element; title: JSX.Element })[];
+}> = (props) => {
+  const service = useMachine(accordion.machine, {
+    id: createUniqueId(),
+    ...props.machineProps,
+  });
 
   const api = createMemo(() => accordion.connect(service, normalizeProps));
 
   return (
     <div {...api().getRootProps()}>
-      <For each={data}>
+      <For each={props.item}>
         {(item) => (
-          <div {...api().getItemProps({ value: item.title })}>
+          <div {...api().getItemProps({ value: item.value })}>
             <h3>
-              <button {...api().getItemTriggerProps({ value: item.title })}>
+              <button {...api().getItemTriggerProps({ value: item.value })}>
                 {item.title}
               </button>
             </h3>
-            <div {...api().getItemContentProps({ value: item.title })}>
+            <div {...api().getItemContentProps({ value: item.value })}>
               {item.content}
             </div>
           </div>
@@ -31,4 +38,4 @@ export function Accordion() {
       </For>
     </div>
   );
-}
+};
